239ed3a6ce754011981c4d4c68195f01605cd58e
Richard K <rkuesters@gmail.com>
2014-08-01 10:16:55 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,15 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "TxMilter",
-    version = "0.1",
-    author = "Jean Marcel Duvoisin Schmidt, Humantech",
-    author_email = "contato@jschmidt.me",
+    name = "txmilter",
+    version = "0.1.0",
+    author = ["Humantech Knowledge Management", "Jean Marcel Duvoisin Schmidt"],
+    author_email = "jean.schmidt@humantech.com.br",
     description = "Twisted protocol to handle Milter Connections in libmilter style",
     license = "GNU GPLv2",
     keywords = "Milter, Twisted",
     url = "https://github.com/humantech/txmilter",
+    install_requires=["twisted"],
     packages=["txmilter"],
     long_description=read("README.md")
 )
